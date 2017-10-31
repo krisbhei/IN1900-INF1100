815e6a98e8ec136b2475c2547e07b705d49634d3
krisbhei <krisbhei@ifi.uio.no>
2017-10-31 09:57:25 +0100

    Update bruke_Circle.py

--- H17/9-class/bruke_Circle.py
+++ H17/9-class/bruke_Circle.py
@@ -1,5 +1,5 @@
-from Circle import *
-
-
+from Circle import * # Her bruker programmet at fila som inneholder klassen Circle også heter Circle.
+                     # Hvis fila med klassen het noe annet, f.eks klasse.py, må programmet bruke
+                     # from klasse import * isteden.
 c = Circle(0,0,4)
 print(c)
